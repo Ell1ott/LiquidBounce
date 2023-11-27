59dac5be472db83f6e0f48b6adce90e5b5c91cf3
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-11-27 13:39:31 +0100

    move delay

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleAutoArmor.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleAutoArmor.kt
@@ -52,6 +52,7 @@ object ModuleAutoArmor : Module("AutoArmor", Category.COMBAT) {
         val bestArmor = findBestArmorPiecesInInventory()
 
         for ((armorIndex, armorPiece) in bestArmor.withIndex()) {
+            wait(inventoryConstraints.delay.random()) { inventoryConstraints.violatesNoMove }
             if (!canOperate(player))
                 break
 
@@ -67,8 +68,6 @@ object ModuleAutoArmor : Module("AutoArmor", Category.COMBAT) {
             if (armorIndex != bestArmor.lastIndex) {
                 // Wait the requested delay, then continue. In case the user violates NoMove,
                 // it immediately goes to the next loop, breaks it and then closes inventory if it's open
-                wait(inventoryConstraints.delay.random()) { inventoryConstraints.violatesNoMove }
-
                 continue
             }
 
