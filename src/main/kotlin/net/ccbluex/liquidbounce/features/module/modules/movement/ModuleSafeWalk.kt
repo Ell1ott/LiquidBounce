8e83c61898dac1042861a81448cbbdb12a17adf2
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-12-05 15:19:22 +0100

    Update ModuleSafeWalk.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleSafeWalk.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/movement/ModuleSafeWalk.kt
@@ -27,9 +27,13 @@ import net.ccbluex.liquidbounce.event.events.PlayerSafeWalkEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.utils.client.chat
+import net.ccbluex.liquidbounce.utils.client.toRadians
 import net.ccbluex.liquidbounce.utils.entity.SimulatedPlayer
 import net.ccbluex.liquidbounce.utils.entity.isCloseToEdge
+import net.ccbluex.liquidbounce.utils.math.minus
 import net.ccbluex.liquidbounce.utils.movement.DirectionalInput
+import net.minecraft.util.math.Vec3d
 
 /**
  * SafeWalk module
@@ -57,27 +61,63 @@ object ModuleSafeWalk : Module("SafeWalk", Category.MOVEMENT) {
 
         private val predict by int("Ticks", 5, 0..20)
 
+        private val smartMoveBack by boolean("SmartMoveBack", true)
+
+        private fun fallingPlayer(simulatedPlayerInput: SimulatedPlayer.SimulatedPlayerInput): Vec3d? {
+            val simulatedPlayer = SimulatedPlayer.fromClientPlayer(simulatedPlayerInput)
+
+            repeat(predict) {
+                simulatedPlayer.tick()
+            }
+
+
+
+            if (simulatedPlayer.fallDistance <= 0.0)
+                return null
+            return simulatedPlayer.pos
+
+        }
+
         /**
          * The input handler tracks the movement of the player and calculates the predicted future position.
          */
         private val inputHandler = handler<MovementInputEvent> { event ->
             if (player.isOnGround && !player.isSneaking) {
-                val simulatedPlayer = SimulatedPlayer.fromClientPlayer(
-                    SimulatedPlayer.SimulatedPlayerInput(
-                        event.directionalInput,
+                val dirInput = event.directionalInput
+                val fallingPlayerPos = fallingPlayer(SimulatedPlayer.SimulatedPlayerInput(
+                    dirInput,
+                    event.jumping,
+                    player.isSprinting,
+                    player.isSneaking
+                )) ?: return@handler
+
+                if(smartMoveBack) {
+                    // If we stop the player, will he still fall down
+                    val fallingPlayerWithoutMoving = fallingPlayer(SimulatedPlayer.SimulatedPlayerInput(
+                        DirectionalInput.NONE,
                         event.jumping,
                         player.isSprinting,
                         player.isSneaking
                     ))
 
-                repeat(predict) {
-                    simulatedPlayer.tick()
+                    fallingPlayerWithoutMoving?.let { pos ->
+                        // If even the stopped player is going to fall down, we want to move backwards to not fall down
+
+                        val diff = fallingPlayerPos - player.pos
+                        val rotatedDiff = diff.rotateY(player.yaw.toRadians())
+
+                        val x = rotatedDiff.x
+
+                        val z = rotatedDiff.z
+
+                        chat("x: $x z: $z")
+
+                        event.directionalInput = DirectionalInput(x < -0.0 , x > 0.0, z < -0.0, z > 0.0)
+                        return@handler
+                        }
                 }
 
-                if (simulatedPlayer.fallDistance > 0.0) {
-                    event.directionalInput = DirectionalInput.NONE
-                    return@handler
-                }
+                event.directionalInput = DirectionalInput.NONE
             }
         }
 
